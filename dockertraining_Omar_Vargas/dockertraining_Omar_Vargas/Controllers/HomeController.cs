66422ebc1f1cadf0a17a80dcada729a25f080a8e
GitJira22 <Git_Jira@brierley.com>
2020-04-05 23:29:22 -0500

    session5 project

---
+++ dockertraining_Omar_Vargas/dockertraining_Omar_Vargas/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dockertraining_Omar_Vargas.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace dockertraining_Omar_Vargas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.EnvVar = _configuration.GetSection("AppSettings").GetSection("StoreName").Value;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ dockertraining_Omar_Vargas/dockertraining_Omar_Vargas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dockertraining_Omar_Vargas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
